970764d7a664863d3c819c5f9b95df24082cc58a
Rodion Matvieiev <matveev.r.v@icloud.com>
2024-02-23 00:38:49 -0500

    update

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { BooksAPI } from './scripts/booksAPI';
-const booksAPI = new BooksAPI();
-
-booksAPI
-  .fetchCategoryList()
-  .then(res => console.log(res[0]))
-  .catch(err => console.log(err));
