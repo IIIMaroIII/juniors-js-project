ca26f99c7107672b3803b3e7e259d6fc1269b2f1
Rodion Matvieiev <matveev.r.v@icloud.com>
2024-02-22 23:11:10 -0500

    update

--- src/scripts/booksAPI.js
+++ src/scripts/booksAPI.js
@@ -1,4 +1,96 @@
+import Axios from 'axios';
+
 export class BooksAPI {
-    constructor()
-    
+  constructor() {
+    this.BASE_URL = 'https://books-backend.p.goit.global/books';
+    this.CATEGORY_LIST = '/category-list';
+    this.TOP_BOOKS = '/top-books';
+    this.URL_CATEGORY = `/category?`;
+  }
+  async fetchCategoryList() {
+    const url = this.BASE_URL + this.CATEGORY_LIST;
+    try {
+      const res = await Axios.get(url);
+      return res.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async fetchTopBooks() {
+    const url = this.BASE_URL + this.TOP_BOOKS;
+    try {
+      const res = await Axios.get(url);
+      return res.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async fetchBookByID(id) {
+    const url = `${this.BASE_URL}/${id}`;
+    try {
+      const res = await Axios.get(url);
+      return res.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async fetchBooksBySelectedCategory(userSelectedCategory) {
+    const queryCategory = new URLSearchParams();
+    queryCategory.append('category', userSelectedCategory);
+    const url = `${this.BASE_URL}${
+      this.URL_CATEGORY
+    }${queryCategory.toString()}`;
+
+    try {
+      const res = await Axios.get(url);
+      return res.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
+
+// List of steps
+
+// Create a prototype of BooksAPI()
+
+const booksAPI = new BooksAPI();
+
+// Method fetchCategoryList returns you a promise, therefore use then().catch() to receive the data of response.
+
+booksAPI
+  .fetchCategoryList()
+  .then(res => console.log(res[0]))
+  .catch(err => console.log(err));
+
+// Method fetchTopBooks returns you a promise, therefore use then().catch() to receive the TOP 5 books of EACH CATEGORY.
+
+booksAPI
+  .fetchTopBooks()
+  .then(res => console.log(res[0].books[0]._id))
+  .catch(err => console.log(err));
+
+// Method fetchBookByID returns you a promise, therefore use then().catch().
+// also this method awaits as argument the ID of chose book as the string.
+
+booksAPI
+  .fetchBookByID('643282b1e85766588626a0dc')
+  .then(res => console.log(res))
+  .catch(err => console.log(err));
+
+// Method fetchBooksBySelectedCategory returns you a promise, therefore use then().catch().
+// also this method awaits as argument the CATEGORY NAME which user has choosen as the string.
+
+booksAPI
+  .fetchBooksBySelectedCategory('Advice How-To and Miscellaneous')
+  .then(res => console.log(res))
+  .catch(err => console.log(err));
+
+// https://books-backend.p.goit.global/books/category-list - все категории
+// https://books-backend.p.goit.global/books/top-books  - топ 5 книг во всех категориях
+// https://books-backend.p.goit.global/books/category?category=selectedCategory - 20 книг выбранной категории
+// https://books-backend.p.goit.global/books/ 'bookId' - детальная инфа про книгу по ID
+// 643282b1e85766588626a0dc
