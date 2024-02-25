694dd3d6742e81d77d9dfd9af4eb85f72b37701f
dmMaksymenko <dmitriy.maksimenko928@gmail.com>
2024-02-25 11:16:34 +0000

    fixed some errors in header.js

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -2,11 +2,13 @@
 const burgerMenu = document.querySelector('.burger-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const useElement = burgerMenu.querySelector('use');
+
 burgerMenu.addEventListener('click', () => {
   mobileMenu.classList.toggle('active');
   if (mobileMenu.classList.contains('active')) {
     useElement.setAttribute('xlink:href', '../img/sprite.svg#icon-x-close');
   } else {
+    burgerMenu.src = './img/burger-menu-icon.svg';
     useElement.setAttribute(
       'xlink:href',
       '../img/sprite.svg#icon-burger-menu-icon'
