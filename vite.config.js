b8d8e762dbce7af04708ec62b3c937cf7d6f38af
Rodion Matvieiev <matveev.r.v@icloud.com>
2024-02-27 02:04:58 -0500

    update

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
-import ViteTerser from 'vite-plugin-terser';
+import ViteTerser from 'terser';
 
 export default defineConfig(({ command }) => {
   return {
